d9dd0a5cc7aea14c0cc1befd16281fb3e3cb6578
Rodrigo Moraes <rodrigovmoraes@gmail.com>
2019-11-27 19:03:42 +0000

    20191127

--- middlewares/purgeCache.js
+++ middlewares/purgeCache.js
@@ -9,16 +9,24 @@ var cachePurgeServiceConfig = config.get("CachePurgeService");
 
 module.exports = function(req, res, next) {
    if (cachePurgeServiceConfig.enabled) {
+      winston.verbose("PURGE CACHE REQUESTED URL = [%s]", cachePurgeServiceConfig.url);
       _requestService({
          'url': cachePurgeServiceConfig.url,
          method: "GET",
          json: true
       }).then(function(data) {
          winston.debug("PURGE CACHE REQUEST OK MESSAGE= [%s]", data.message);
+         if (cachePurgeServiceConfig.refreshEnable) {
+            winston.debug("REFRESH CACHE REQUESTED URL = [%s]", cachePurgeServiceConfig.refreshUrl);
+            return _requestService({
+               'url': cachePurgeServiceConfig.refreshUrl,
+               method: "GET",
+               json: true
+            });
+         }
       }).catch(function(error) {
          winston.debug("PURGE CACHE REQUEST ERROR MESSAGE= [%s]", error.message);
       });
-      winston.verbose("PURGE CACHE REQUESTED URL = [%s]", cachePurgeServiceConfig.url);
    }
    next();
 }
